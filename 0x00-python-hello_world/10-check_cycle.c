2fab9418e5fd94f99b74c86ec0115857fde2b6e3
mi-kei-la <micaela.percovich@gmail.com>
2020-12-07 20:31:51 +0000

    add file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+  * check_cycle - check if there is a loop in a linked list
+  *
+  * @list: pointer to head
+  *
+  * Return: 1 if loop, 0 if no loop
+  */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *tmp = list;
+	listint_t *fast_and_furious = list;
+
+	while (fast_and_furious != NULL)
+	{
+		if (tmp->next == fast_and_furious->next->next)
+			return (1);
+		tmp = tmp->next;
+		fast_and_furious = fast_and_furious->next->next;
+	}
+	return (0);
+}
