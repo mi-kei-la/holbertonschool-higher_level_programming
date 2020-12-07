31f14ad0d8a4ba6c6c28e215180fa4b815a9c741
mi-kei-la <micaela.percovich@gmail.com>
2020-12-07 21:44:52 +0000

    add comments

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
 b = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-sys.stderr.write(b)
-sys.exit(1)
+sys.stderr.write(b)  # write var to stderr
+sys.exit(1)  # exit code
