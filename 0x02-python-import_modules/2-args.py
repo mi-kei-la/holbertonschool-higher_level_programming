588c35158ed2fd32c0982c7b361fa0d9985e7758
mi-kei-la <micaela.percovich@gmail.com>
2020-12-10 17:04:55 +0000

    update statements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,16 @@
 import sys
 if __name__ == "__main__":
     l = len(sys.argv)
-    if l > 1:
-        print("{} arguments:" .format(l - 1))
+    if l is 1:
+        print("0 arguments.")
+    elif l is 2:
+        count = 1
+        print("{} argument:" .format(l - 1))
+        print("{:d}: {:s}" .format(count, sys.argv[count]))
+    elif l > 2:
+        print("{} arguments:" .format(l))
         count = 0
         for a in sys.argv:
             if count >= 1:
                 print("{:d}: {:s}" .format(count, sys.argv[count]))
             count = count + 1
-    else:
-        print("0 arguments.")
