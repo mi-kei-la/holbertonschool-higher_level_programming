a278c101032e2ee358882448803861098a871523
mi-kei-la <micaela.percovich@gmail.com>
2020-12-10 17:28:28 +0000

    add

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,10 +7,7 @@ if __name__ == "__main__":
         print("{:d}: {:s}" .format(l, sys.argv[l]))
     elif l > 1:
         print("{} arguments:" .format(l))
-        count = 0
-        for a in sys.argv:
-            if count > 1:
-                print("{:d}: {:s}" .format(count, sys.argv[count]))
-            count = count + 1
+        for a in range(1, len(sys.argv)):
+            print("{:d}: {:s}" .format(a, sys.argv[a]))
     else:
         print("0 arguments.")
