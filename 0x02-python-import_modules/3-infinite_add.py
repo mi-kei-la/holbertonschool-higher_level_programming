2a8353746482fd30387a3e8f202cbba8d5de6a8d
mi-kei-la <micaela.percovich@gmail.com>
2020-12-10 17:36:33 +0000

    add condition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
-x = 0
-for i in range(1, len(sys.argv)):
-    x = x + int(sys.argv[i])
-print(x)
+if __name__ == "__main__":
+    x = 0
+    for i in range(1, len(sys.argv)):
+        x = x + int(sys.argv[i])
+    print(x)
