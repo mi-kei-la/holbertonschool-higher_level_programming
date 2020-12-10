5c27936be3709d49e9d5f78b02f16a631f0949fd
mi-kei-la <micaela.percovich@gmail.com>
2020-12-10 19:39:20 +0000

    upload advanced task

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    avlen = len(sys.argv)
+    sign = str(sys.argv[2])
+
+    if avlen != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif sign != '+' and sign != '-' and sign != '/' and sign != '\*':
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if sign == '+':
+        print("{} + {} = {}" .format(a, b, add(a, b)))
+        exit(0)
+    elif sign == '-':
+        print("{} - {} = {}" .format(a, b, sub(a, b)))
+        exit(0)
+    elif sign == '\*':
+        print("{} * {} = {}" .format(a, b, mul(a, b)))
+        exit(0)
+    elif sign == '/':
+        print("{} / {} = {}" .format(a, b, sub(a, b)))
+        exit(0)
