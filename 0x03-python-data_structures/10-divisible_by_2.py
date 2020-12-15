b2d0b9004f138140ea7d15b0dd45ee404f5d6792
mi-kei-la <micaela.percovich@gmail.com>
2020-12-15 21:39:57 +0000

    add new files

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list:
+        new = []
+        for x in my_list:
+            if x % 2 == 0:
+                new.append(True)
+            else:
+                new.append(False)
+        return new
+    else:
+        return None
