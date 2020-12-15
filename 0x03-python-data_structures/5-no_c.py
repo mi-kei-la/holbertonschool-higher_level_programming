625ad68bdddc58b4408b5fed9b1da453fd2afa44
mi-kei-la <micaela.percovich@gmail.com>
2020-12-15 18:35:02 +0000

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if (my_string):
-        new = ""
-        for x in my_string:
-            if x != "C" and x != "c":
-                new = new + x
-        return new
+    new = ""
+    for x in my_string:
+        if x != "C" and x != "c":
+            new = new + x
+    return new
