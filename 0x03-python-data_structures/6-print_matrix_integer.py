9a36b0e24acf160c18b1b294d697287de6e23791
mi-kei-la <micaela.percovich@gmail.com>
2020-12-15 19:07:46 +0000

    upload file

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix == "":
+        print("")
+    for row in matrix:
+        a = 0
+        for value in row:
+            a = a + 1
+            if a < len(row):
+                print("{:d}" .format(value), end=" ")
+            else:
+                print("{:d}" .format(value))
