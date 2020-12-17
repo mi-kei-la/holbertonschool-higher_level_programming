6406fbffe647f4ad39e862b0e59c3e1433de0258
mi-kei-la <micaela.percovich@gmail.com>
2020-12-17 21:25:06 +0000

    add task 5

--- 0x04-python-more_data_structures/4-main.py
+++ 0x04-python-more_data_structures/4-main.py
@@ -5,3 +5,13 @@ set_1 = { "Python", "C", "Javascript" }
 set_2 = { "Bash", "C", "Ruby", "Perl" }
 od_set = only_diff_elements(set_1, set_2)
 print(sorted(list(od_set)))
+
+set_1 = { "Bash", "C" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+od_set = only_diff_elements(set_1, set_2)
+print(sorted(list(od_set)))
+
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Python", "C", "Javascript" }
+od_set = only_diff_elements(set_1, set_2)
+print(sorted(list(od_set)))
