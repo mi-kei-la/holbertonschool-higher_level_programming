c059cee2995a47f9c64e9d8329a7efc8a3340b63
mi-kei-la <micaela.percovich@gmail.com>
2020-12-18 13:55:04 +0000

    update

--- 0x04-python-more_data_structures/12-main.py
+++ 0x04-python-more_data_structures/12-main.py
@@ -5,20 +5,21 @@ roman_to_int = __import__('12-roman_to_int').roman_to_int
 
 roman_number = "X"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-print("this should be 10")
 
 roman_number = "VII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-print("this should be 7")
 
 roman_number = "IX"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-print("this should be 9")
 
 roman_number = "LXXXVII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-print("this should be 87")
 
 roman_number = "DCCVII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-print("this should be 707")
+
+roman_number = "I"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "III"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
