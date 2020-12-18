87ff67e9e2ae18511f8dc98e4fd0d51e0007b131
mi-kei-la <micaela.percovich@gmail.com>
2020-12-18 00:17:16 +0000

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        r = sorted(a_dictionary.items())
-        r.reverse()
-        return r[0][0]
+        return max(a_dictionary, key = lambda k: a_dictionary[k])
