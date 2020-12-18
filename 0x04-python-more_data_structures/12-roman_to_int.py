ebd0821833512c7f2d8f95a13a55c921c1fd6da7
mi-kei-la <micaela.percovich@gmail.com>
2020-12-18 13:57:09 +0000

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) == str and roman_string is not None:
+    if type(roman_string) == str and roman_string:
         romans = {'M': 1000, 'D': 500, 'C': 100}
         l = {'L': 50, 'X': 10, 'V': 5, 'I': 1}
         romans.update(l)
@@ -18,3 +18,5 @@ def roman_to_int(roman_string):
                     total = total + nums[i]
         total = total + nums[c]
         return total
+    else:
+        return None
