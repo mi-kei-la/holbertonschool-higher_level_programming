b63764d6725341410fe445022ce390b1f32b1e98
mi-kei-la <micaela.percovich@gmail.com>
2021-01-04 17:40:02 +0000

    update

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
     except:
         print("Unexpected error:", sys.exc_info()[0])
