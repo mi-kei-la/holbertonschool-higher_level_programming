e4834c0fe8b3500721cafe55ae3bc762b7991222
mi-kei-la <micaela.percovich@gmail.com>
2021-01-05 22:53:11 +0000

    update docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """This is the docstring for an empty class.
-
-    This class has no attributes.
-
-    """
+    """This is the docstring for an empty class."""
     pass
