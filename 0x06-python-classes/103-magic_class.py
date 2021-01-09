a203182c82f6a3dba2ce60771ebb5c5bbf3768c6
mi-kei-la <micaela.percovich@gmail.com>
2021-01-09 03:30:43 +0000

    update comments

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""This class mimics bytecode.
-The objective of this is to
-
-"""
+"""This class mimics bytecode."""
 
 
 class MagicClass:
     def __init__(self, radius=0):
+        """
+        initialize bytecode.
+        """
         self._MagicClass__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self._MagicClass__radius = radius
 
-
     def area(self):
+        """calculate area"""
         return self._MagicClass__radius ** 2 * math.pi
 
-
     def circumference(self):
+        """calculate circumference"""
         return 2 * math.pi * self._MagicClass__radius
