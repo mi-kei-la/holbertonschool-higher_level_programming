82749abd5ce3a9b1dcc93c41a4f1a295a7d4502e
mi-kei-la <micaela.percovich@gmail.com>
2021-01-17 01:37:21 +0000

    update code

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,34 +1,46 @@
 #!/usr/bin/python3
-"""This is a single function module.
+"""
+This is a single function module.
 Print any given text with empty lines instead of special characters.
+Special characters: period (.), colon (:), question mark (?).
 
 """
 
 
 def text_indentation(text):
     """
-    The function takes a string and prints it, replacing each period (.),\
-    double colon (:) and question mark (?) for two empty lines.
+    The function takes a string and prints it, replacing each period (.),
+    colon (:) and question mark (?) for two empty lines.
 
     Args:
-        text (str): text to be printed
+        text (str): text to be printed.
 
     Raises:
-        TypeError: when text is not a string
+        TypeError: when text is not a string.
     """
+    # Check parameter type (must be a string).
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    flag = False
+    # Make list of special characters to search.
+    chars = ['.', ':', '?']
+    # Convert text to list.
+    listext = list(text)
 
-    for x in text:
-        if (x is not '.' and x is not ':' and x is not '?'):
-            if flag is False:
-                print(x, end="")
-            else:
-                flag = False
-                continue
-        else:
-            print(x)
-            print()
-            flag = True
+    for c in chars:
+        idx = 0
+        spc = 0
+        end = listext.count(c)
+        while end:
+            idx = listext.index(c, idx)
+            if ' ' in listext:
+                spc = listext.index(' ', idx)
+                if idx + 1 == spc:
+                    listext.pop(spc)
+            listext.insert(idx + 1, "\n\n")
+            idx += 3
+            end -= 1
+
+    # Print final text.
+    print("".join(listext))
+
