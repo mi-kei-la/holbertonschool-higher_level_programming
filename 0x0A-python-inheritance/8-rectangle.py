ec246f57a75751a71e4b3fe04134ef0d49881fb5
mi-kei-la <micaela.percovich@gmail.com>
2021-01-19 22:19:51 +0000

    import code instead of repeating it

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,41 +1,6 @@
 #!/usr/bin/python3
-"""This module contains two classes.
-
-Class:
-    BaseGeometry: parent class.
-    Rectangle: defines a rectangle.
-"""
-
-
-class BaseGeometry():
-    """This class was not left empty.
-
-    Instead, it has a method which raises an exception, \
-    and a method to validate values.
-    """
-    def area(self):
-        """Area is not implemented yet, so an exception is raised."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates the value parameter.
-
-        Args:
-            name: always a string.
-            value: positive integer.
-
-        Raises:
-            TypeError:  if value is not an integer.
-                        if name is not a string
-            ValueError: if value is less or equal to 0.
-        """
-        if type(name) != str:
-            raise TypeError("name must be a string")
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-        pass
+"""This is a single module class."""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
