e69dff0774b6eb88f5c089cb4a079ee6a8263e80
mi-kei-la <micaela.percovich@gmail.com>
2021-01-26 16:47:12 +0000

    update style of test files, create test folder

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Test cases for all attributes and methods of the Base class."""
-import unittest
-from models.base import Base
-
-
-class TestBase(unittest.TestCase):
-    """Test base class and its methods."""
