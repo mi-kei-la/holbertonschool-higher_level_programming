25b8435f0d551670cdcb213324b0675aebfa7051
mi-kei-la <micaela.percovich@gmail.com>
2021-03-01 19:39:23 +0000

    update id

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- Create table and insert data.
-CREATE TABLE IF NOT EXISTS second_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256), score INT);
-INSERT INTO second_table (name, score) VALUES ('Jhon', 10);
-INSERT INTO second_table (name, score) VALUES ('Alex', 3);
-INSERT INTO second_table (name, score) VALUES ('Bob', 14);
-INSERT INTO second_table (name, score) VALUES ('George', 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, 'Jhon', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
 
