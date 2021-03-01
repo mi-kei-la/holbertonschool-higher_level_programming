fb86572692536817081319d1b8879057a8b8fc06
mi-kei-la <micaela.percovich@gmail.com>
2021-03-01 19:33:05 +0000

    update code

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,7 @@
 -- Create table and insert data.
 CREATE TABLE IF NOT EXISTS second_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256), score INT);
-INSERT INTO second_table (name, score) VALUES ('Jhon', 10), ('Alex', 3), ('Bob', 14), ('George', 8);
+INSERT INTO second_table (name, score) VALUES ('Jhon', 10);
+INSERT INTO second_table (name, score) VALUES ('Alex', 3);
+INSERT INTO second_table (name, score) VALUES ('Bob', 14);
+INSERT INTO second_table (name, score) VALUES ('George', 8);
 
