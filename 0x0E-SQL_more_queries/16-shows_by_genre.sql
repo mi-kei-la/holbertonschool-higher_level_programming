05479d5935d773301c85fc056858f622d7c7e2d2
mi-kei-la <micaela.percovich@gmail.com>
2021-03-02 21:49:39 +0000

    add task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,18 @@
+-- Display all shows with their genre name.
+SELECT
+	tv_shows.title AS title,
+	tv_genres.name AS name
+FROM
+	tv_shows
+LEFT JOIN
+	tv_show_genres
+ON
+	tv_show_genres.show_id = tv_shows.id
+LEFT JOIN
+	tv_genres
+ON
+	tv_show_genres.genre_id = tv_genres.id
+ORDER BY
+	tv_shows.title,
+	tv_genres.name;
+
