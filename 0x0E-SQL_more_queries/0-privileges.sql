1e2ec7d80ec0813fd586ea92301ebf52dd644444
mi-kei-la <micaela.percovich@gmail.com>
2021-03-02 17:47:05 +0000

    fix syntax

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- This script shows all privileges of two users on localhost server.
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
