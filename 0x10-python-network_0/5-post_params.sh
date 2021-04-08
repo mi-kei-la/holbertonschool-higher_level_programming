0ab13a37072169f62445ab4630e67f00b27c644d
mi-kei-la <micaela.percovich@gmail.com>
2021-04-08 18:07:41 +0000

    update parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a POST request to a given URL.
-curl -s -d "email=hr@holbertonschool.com" subject="I will always be here for PLD" "$1"
+curl -s -d "email=hr@holbertonschool.com" -d subject="I will always be here for PLD" "$1"
