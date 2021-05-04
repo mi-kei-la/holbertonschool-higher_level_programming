6d8a3294f217f3146dd554aac3be339abc68e083
mi-kei-la <micaela.percovich@gmail.com>
2021-05-04 16:44:21 +0000

    NO ENTIENDO

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (error, response) {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  console.log('code: ', response.statusCode);
+request.get(url)
+  .on('response', function(response) {
+    console.log('code: ', response.statusCode);
 });
