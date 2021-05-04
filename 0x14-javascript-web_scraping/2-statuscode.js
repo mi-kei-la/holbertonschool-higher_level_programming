baf6430f27268e046773026eafe1c8013ea10f67
mi-kei-la <micaela.percovich@gmail.com>
2021-05-04 16:35:14 +0000

    remove unused variable

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url, function (error, response, body) {
+request.get(url, function (error, response) {
   if (error) {
     console.log(error);
     return;
