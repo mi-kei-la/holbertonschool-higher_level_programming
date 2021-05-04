740f8d03f9b7de1fc24d7cb6fb7c6b0da9f22e6e
mi-kei-la <micaela.percovich@gmail.com>
2021-05-04 16:38:06 +0000

    remove error handling

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,9 +3,5 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, function (error, response) {
-  if (error) {
-    console.log(error);
-    return;
-  }
   console.log('code: ', response.statusCode);
 });
