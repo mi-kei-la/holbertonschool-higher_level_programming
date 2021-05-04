432a254d9c56fd53b505d79c790806857a09f37e
mi-kei-la <micaela.percovich@gmail.com>
2021-05-04 16:46:15 +0000

    remove variable

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
 
-request.get(url)
+request.get(process.argv[2])
   .on('response', function (response) {
     console.log('code: ', response.statusCode);
   });
